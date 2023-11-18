140104208a5c2cbbc46d9229c1dfc54225cd0091
Stevie <djstevie91@live.com>
2023-11-18 00:10:39 +0000

    Delete config.lua

--- config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-
-Config.pathToServerData = io.popen("cd"):read('*l') -- Get the path to the directory where server.cfg resides (only for windows)
-Config.resourceName = GetCurrentResourceName() -- Get the name of our resource so we can get the full path
-Config.fullPathToResource = Config.pathToServerData .. '\\resources\\' .. Config.resourceName
